a0e54e52d6d71e3aeaed42288a7437ead93d2e3b
AbcTheHuman2 <athehuman@gmail.com>
2017-04-22 16:59:10 -0300

    TrabalhoPOO

---
+++ TrabalhoPOO/app/src/main/java/com/example/gabri/trabalhopoo/Alterar.java
@@ -0,0 +1,41 @@
+package com.example.gabri.trabalhopoo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Alterar extends Activity {
+    EditText livro;
+    EditText autor;
+    EditText editora;
+    Button alterar;
+    Button deletar;
+    Cursor cursor;
+    BancoController crud;
+    String codigo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar);
+
+        codigo = this.getIntent().getStringExtra("codigo");
+
+        crud = new BancoController(getBaseContext());
+
+        livro = (EditText) findViewById(R.id.editText4);
+        autor = (EditText) findViewById(R.id.editText5);
+        editora = (EditText) findViewById(R.id.editText6);
+
+        alterar = (Button) findViewById(R.id.button2);
+
+        cursor = crud.carregaDadoById(Integer.parseInt(codigo));
+        livro.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.TITULO)));
+        autor.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.AUTOR)));
+        editora.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.EDITORA)));
+    }
+}
---
+++ TrabalhoPOO/app/src/main/java/com/example/gabri/trabalhopoo/BancoController.java
@@ -0,0 +1,112 @@
+package com.example.gabri.trabalhopoo;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.view.View;
+import android.widget.Button;
+
+public class BancoController {
+
+    private SQLiteDatabase db;
+    private CriaBanco banco;
+
+    public BancoController(Context context){
+        banco = new CriaBanco(context);
+    }
+
+    public String insereDado(String titulo, String autor, String editora){
+        ContentValues valores;
+        long resultado;
+
+        db = banco.getWritableDatabase();
+        valores = new ContentValues();
+        valores.put(CriaBanco.TITULO, titulo);
+        valores.put(CriaBanco.AUTOR, autor);
+        valores.put(CriaBanco.EDITORA, editora);
+
+        resultado = db.insert(CriaBanco.TABELA, null, valores);
+        db.close();
+
+        if (resultado ==-1)
+            return "Erro ao inserir registro";
+        else
+            return "Registro Inserido com sucesso";
+
+    }
+
+    public Cursor carregaDados(){
+        Cursor cursor;
+        String[] campos =  {banco.ID,banco.TITULO};
+        db = banco.getReadableDatabase();
+        cursor = db.query(banco.TABELA, campos, null, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    public Cursor carregaDadoById(int id){
+        Cursor cursor;
+        String[] campos =  {banco.ID,banco.TITULO,banco.AUTOR,banco.EDITORA};
+        String where = CriaBanco.ID + "=" + id;
+        db = banco.getReadableDatabase();
+        cursor = db.query(CriaBanco.TABELA,campos,where, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    public void alteraRegistro(int id, final String titulo, final String autor, final String editora){
+        ContentValues valores;
+        String where;
+
+        db = banco.getWritableDatabase();
+
+        where = CriaBanco.ID + "=" + id;
+
+        valores = new ContentValues();
+        valores.put(CriaBanco.TITULO, titulo);
+        valores.put(CriaBanco.AUTOR, autor);
+        valores.put(CriaBanco.EDITORA, editora);
+
+        db.update(CriaBanco.TABELA,valores,where,null);
+        db.close();
+
+        alterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alteraRegistro(Integer.parseInt(codigo), titulo.getText().toString(),autor.getText().toString(),
+                        editora.getText().toString());
+                Intent intent = new Intent(alterar.this,Consulta.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void deletaRegistro(int id){
+        String where = CriaBanco.ID + "=" + id;
+        db = banco.getReadableDatabase();
+        db.delete(CriaBanco.TABELA,where,null);
+        db.close();
+
+        deletar = (Button)findViewById(R.id.button3);
+        deletar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deletaRegistro(Integer.parseInt(codigo));
+                Intent intent = new Intent(deletar.this,Consulta.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
