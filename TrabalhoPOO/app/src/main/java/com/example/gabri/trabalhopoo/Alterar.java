098911fd99b6baf635b7f2978c9c481a7d275cfe
AbcTheHuman2 <athehuman@gmail.com>
2017-04-22 21:21:30 -0300

    Alterar.java

--- TrabalhoPOO/app/src/main/java/com/example/gabri/trabalhopoo/Alterar.java
+++ TrabalhoPOO/app/src/main/java/com/example/gabri/trabalhopoo/Alterar.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class Alterar extends Activity {
     EditText livro;
     EditText autor;
@@ -21,21 +22,32 @@ public class Alterar extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alterar);
+        setContentView(com.example.gabri.trabalhopoo.R.layout.alterar_dados);
 
         codigo = this.getIntent().getStringExtra("codigo");
 
         crud = new BancoController(getBaseContext());
 
-        livro = (EditText) findViewById(R.id.editText4);
-        autor = (EditText) findViewById(R.id.editText5);
-        editora = (EditText) findViewById(R.id.editText6);
+        livro = (EditText)findViewById(com.example.gabri.trabalhopoo.R.id.editText4);
+        autor = (EditText)findViewById(com.example.gabri.trabalhopoo.R.id.editText5);
+        editora = (EditText)findViewById(com.example.gabri.trabalhopoo.R.id.editText6);
 
-        alterar = (Button) findViewById(R.id.button2);
+        alterar = (Button)findViewById(com.example.gabri.trabalhopoo.R.id.button2);
 
         cursor = crud.carregaDadoById(Integer.parseInt(codigo));
         livro.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.TITULO)));
         autor.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.AUTOR)));
         editora.setText(cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.EDITORA)));
+
+        alterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crud.alteraRegistro(Integer.parseInt(codigo), livro.getText().toString(),autor.getText().toString(),
+                        editora.getText().toString());
+                Intent intent = new Intent(Alterar.this,Consulta.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
